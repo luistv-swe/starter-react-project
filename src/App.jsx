0529d180bf7a7d4ec7f1a7291746ff19645df3fd
Luis Terrones-Verastegui <lterrones316@gmail.com>
2024-05-24 15:32:09 -0700

    Use a data.js file and object spreading/destructuring to efficiently display 4 CoreConcept components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import reactLogo from './assets/react-core-concepts.png';
 import componentsImg from './assets/components.png';
+import { CORE_CONCEPTS } from './data';
 
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
@@ -7,12 +8,12 @@ function genRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
 }
 
-function CoreConcept(props) {
+function CoreConcept({image, title, description}) {
   return (
     <li>
-      <img src={props.image} alt={props.title}/>
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
+      <img src={image} alt={title}/>
+      <h3>{title}</h3>
+      <p>{description}</p>
     </li>
   );
 }
@@ -39,10 +40,10 @@ function App() {
         <section id='core-concepts'>
         <h2>Core Concepts</h2>
         <ul>
-          <CoreConcept
-            title="Components"
-            description="The core UI building block"
-            image={componentsImg} />
+          <CoreConcept {...CORE_CONCEPTS[0]}/>
+          <CoreConcept {...CORE_CONCEPTS[1]}/>
+          <CoreConcept {...CORE_CONCEPTS[2]}/>
+          <CoreConcept {...CORE_CONCEPTS[3]}/>
         </ul>
         </section>
       </main>
