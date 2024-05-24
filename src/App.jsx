f616f2b208a63ed6ea9d2e5987e2eefdc7d89d55
Luis Terrones-Verastegui <lterrones316@gmail.com>
2024-05-24 15:23:52 -0700

    Create a component that uses props

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import reactLogo from './assets/react-core-concepts.png';
+import componentsImg from './assets/components.png';
 
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
 function genRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
+}
+
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.title}/>
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
 }
 
 function Header() {
@@ -25,7 +36,15 @@ function App() {
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id='core-concepts'>
+        <h2>Core Concepts</h2>
+        <ul>
+          <CoreConcept
+            title="Components"
+            description="The core UI building block"
+            image={componentsImg} />
+        </ul>
+        </section>
       </main>
     </div>
   );
