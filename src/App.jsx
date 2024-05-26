4b93ba0afb93cd72b5a51d3d1ec0a70ae2bb90dc
Luis Terrones-Verastegui <lterrones316@gmail.com>
2024-05-26 12:05:33 -0700

    Add TabButton component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import componentsImg from './assets/components.png';
 import { CORE_CONCEPTS } from './data';
 import CoreConcept from './components/CoreConecpt';
 import Header from './components/Header';
+import TabButton from './components/TabButton';
 
 function App() {
   return (
@@ -16,6 +17,15 @@ function App() {
           <CoreConcept {...CORE_CONCEPTS[2]}/>
           <CoreConcept {...CORE_CONCEPTS[3]}/>
         </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
+          </menu>
         </section>
       </main>
     </div>
