2efcc2f7ea3d7c286b17b78fc32a9ffbe7a7c2d4
Luis Terrones-Verastegui <lterrones316@gmail.com>
2024-05-26 12:20:27 -0700

    Add onClick listener to TabButton that prints button identifier to console

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ import Header from './components/Header';
 import TabButton from './components/TabButton';
 
 function App() {
+  function handleSelect(selectedButton) {
+    console.log(selectedButton);
+  }
+
   return (
     <div>
       <Header />
@@ -21,10 +25,10 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton>Components</TabButton>
-            <TabButton>JSX</TabButton>
-            <TabButton>Props</TabButton>
-            <TabButton>State</TabButton>
+            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
         </section>
       </main>
