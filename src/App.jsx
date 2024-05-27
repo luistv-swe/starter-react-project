dc3e32332b5feff992d39b2e8bac83137c526024
Luis Terrones-Verastegui <lterrones316@gmail.com>
2024-05-27 14:48:55 -0700

    Refactor App.jsx to use separate components for each section

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,17 @@
-import componentsImg from './assets/components.png';
-import { CORE_CONCEPTS } from './data';
-import CoreConcept from './components/CoreConecpt';
 import Header from './components/Header';
-import TabButton from './components/TabButton';
-import { useState } from 'react';
-import { EXAMPLES } from './data';
+import CoreConcepts from './components/CoreConcepts';
+import Examples from './components/Examples';
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState();
-
-  function handleSelect(selectedButton) {
-    setSelectedTopic(selectedButton);
-  }
-
-  let tabContent = <p>Please select a topic.</p>;
-  if (selectedTopic) {
-    tabContent = <div id="tab-content">
-      <h3>{EXAMPLES[selectedTopic].title}</h3>
-      <p>{EXAMPLES[selectedTopic].description}</p>
-      <pre>
-        <code>
-          {EXAMPLES[selectedTopic].code}
-        </code>
-      </pre>
-    </div>
-  }
 
   return (
-    <div>
+    <>
       <Header />
       <main>
-        <section id='core-concepts'>
-          <h2>Core Concepts</h2>
-          <ul>
-            {CORE_CONCEPTS.map(
-              (conceptItem) => <CoreConcept key={conceptItem.title} {...conceptItem}/>
-            )}
-          </ul>
-        </section>
-        <section id="examples">
-          <h2>Examples</h2>
-          <menu>
-            <TabButton isSelected={selectedTopic === 'components'} onSelect={() => handleSelect('components')}>Components</TabButton>
-            <TabButton isSelected={selectedTopic === 'jsx'} onSelect={() => handleSelect('jsx')}>JSX</TabButton>
-            <TabButton isSelected={selectedTopic === 'props'} onSelect={() => handleSelect('props')}>Props</TabButton>
-            <TabButton isSelected={selectedTopic === 'state'} onSelect={() => handleSelect('state')}>State</TabButton>
-          </menu>
-          {tabContent}
-        </section>
+        <CoreConcepts />
+        <Examples />
       </main>
-    </div>
+    </>
   );
 }
 
---
+++ src/components/CoreConcepts.jsx
@@ -0,0 +1,14 @@
+import { CORE_CONCEPTS } from "../data";
+import CoreConcept from "./CoreConecpt";
+
+export default function CoreConcepts() {
+    return (
+        <section id='core-concepts'>
+            <h2>Core Concepts</h2>
+            <ul>
+                {CORE_CONCEPTS.map(
+                    (conceptItem) => <CoreConcept key={conceptItem.title} {...conceptItem} />
+                )}
+            </ul>
+        </section>);
+}
